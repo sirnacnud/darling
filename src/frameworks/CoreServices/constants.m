f3f1d1c833fed75dbcf782982220078e25d94331
Duncan Cunningham <duncanc4@gmail.com>
2025-06-23 16:42:12 +0200

    Add private strings in CoreServices

--- src/frameworks/CoreServices/constants.m
+++ src/frameworks/CoreServices/constants.m
@@ -7,3 +7,7 @@ const char* const CoreServicesVersionString = "@(#)PROGRAM:CoreServices  PROJECT
 // this probably shouldn't be here
 const CFStringRef kFSOperationBytesCompleteKey = CFSTR("kFSOperationBytesDoneKey");
 const CFStringRef kFSOperationTotalBytesKey = CFSTR("kFSOperationTotalBytesKey");
+
+const CFStringRef _kLSLaunchModifierDataTypeKey = CFSTR("_kLSLaunchModifierDataTypeKey");
+const CFStringRef _kLSLaunchModifierDataTypeAppleEventKey = CFSTR("_kLSLaunchModifierDataTypeAppleEventKey");
+const CFStringRef _kLSLaunchModifierDataKey = CFSTR("_kLSLaunchModifierDataKey");
